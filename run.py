079efe23ef9c175630133deb7c4d692ab463e97c
marde12345 <mardefasma123up@gmail.com>
2019-12-03 06:10:23 +0700

    convert done

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
-from flask import Flask,request,jsonify,make_response, after_this_request
+import datetime
+
+from flask import Flask, request, jsonify, make_response, after_this_request
 import json
 import os
 from flask import request
 from os import remove
 from werkzeug import secure_filename
 from flask import Flask, redirect, url_for
+from datetime import datetime
 import json
 from PIL import Image
 
 app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 def checkToken(token):
     """
@@ -31,7 +36,7 @@ def scale():
         scale = jsons['scale']
         ext = jsons['ext']
     except:
-        return json.dumps({'status':'error, argument not satisfied', "image": ""})
+        return json.dumps({'status': 'error, argument not satisfied', "image": ""})
 
     height = image.height * scale
     width = image.height * scale
@@ -48,11 +53,50 @@ def scale():
             image.close()
             os.remove(a)
         except Exception as error:
-                app.logger.error("Error removing or closing downloaded file handle", error)
+            app.logger.error("Error removing or closing downloaded file handle", error)
+
     return ret
+
+
+@app.route("/api/convert", methods=['GET', 'POST'])
+def convert():
+    """
+        convert img to another ext
+        {“ext” : float, “token”: string, “image” : file, "filename" : string}
+        """
+
+    def allowed_file(filename):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    if request.method == 'GET':
+        return jsonify(status='OK', message='HI :)')
+    else:
+        file = request.files['image']
+
+        if not (file and allowed_file(file.filename)):
+            return jsonify(status='Bad Request', message='File not allowed')
+
+        filename = secure_filename(file.filename)
+        uploadpath = os.path.join('upload', datetime.now().strftime("%H%M%S") + filename)
+        file.save(uploadpath)
+
+        ext = request.form.get('ext')
+
+        # Convert
+        img = Image.open(uploadpath)
+        img = img.convert("RGB")
+        filenamedownload = datetime.now().strftime("%H%M%S") + '.' + ext
+        downloadpath = os.path.join('download', filenamedownload)
+        img.save(downloadpath)
+
+        downloadlink = os.path.join(request.url_root, 'download', filenamedownload)
+
+        return jsonify(status='OK', message='berhasil', downloadlink=downloadlink)
 
 
 if __name__ == '__main__':
     from gevent.pywsgi import WSGIServer
-    http_server = WSGIServer(('',5000),app)
+
+    http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
